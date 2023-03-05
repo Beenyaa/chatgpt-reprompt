3d3a1c3590b2fbeb5e365c4bf0ee8a92a414bd25
Bence Gadanyi <29914644+Beenyaa@users.noreply.github.com>
2023-03-05 22:08:36 +0000

    chore: moves code to src & adds gitignore

--- src/content.js
+++ src/content.js
@@ -23,21 +23,43 @@ contentHeaderContainer.classList.add(
   "items-center"
 );
 contentHeaderContainer.style.paddingBottom = "10px";
+contentHeaderContainer.style.paddingTop = "10px";
 contentContainer.appendChild(contentHeaderContainer);
 
 // Add title
 const title = document.createElement("h2");
+title.classList.add(
+  "flex",
+  "items-center"
+);
 title.textContent = "RePrompt";
 title.style.color = "#fff";
 title.style.margin = "0";
 title.style.fontSize = "1.5rem";
 contentHeaderContainer.appendChild(title);
 
+// Add logo
+const logo = document.createElement("span");
+logo.innerHTML = `
+<svg width="32px" height="32px" viewBox="0 0 1280 1280" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g transform="matrix(9.97755,0,0,9.97755,-108.316,-438.823)">
+        <g id="SVGRepo_iconCarrier">
+            <path d="M92.12,99.87L92.12,111.94L84.69,112.36L84.69,85.07C84.69,73.15 92.45,67.73 99.78,67.73C107.43,67.73 115.09,73.53 115.09,83.59C115.09,99.33 98.61,99.87 92.12,99.87ZM99.69,75.43C96.05,75.43 92.16,77.96 92.16,85.05L92.16,92.28C107.31,92.28 107.31,86.85 107.31,83.6C107.31,78 103.36,75.43 99.69,75.43ZM58.24,99.88C51.66,99.88 34.91,99.88 34.91,83.88C34.91,73.65 42.69,67.76 50.46,67.76C57.91,67.76 65.8,73.27 65.8,85.38L65.8,92.38L80.51,92.49L79.82,99.94L58.24,99.88ZM58.24,85.35C58.24,78.02 54.24,75.41 50.46,75.41C46.68,75.41 42.59,78.05 42.59,83.85C42.59,87.21 42.59,92.28 58.24,92.32L58.24,85.35ZM58.24,116.29L58.24,104.9L65.8,103.9L65.8,130.9C65.8,143 57.91,148.52 50.46,148.52C42.69,148.52 34.91,142.62 34.91,132.4C34.91,116.34 51.66,116.29 58.24,116.29ZM50.46,140.77C54.23,140.77 58.24,138.16 58.24,130.83L58.24,123.83C42.59,123.88 42.59,128.95 42.59,132.31C42.59,138.13 46.67,140.77 50.46,140.77ZM113.6,131.77C113.6,141.32 106.34,146.77 99.08,146.77C92.08,146.77 84.76,141.63 84.76,130.32L84.76,123.8L70.45,123.8L69.96,116.2L91.82,116.2C98,116.29 113.6,116.88 113.6,131.8L113.6,131.77ZM92.13,130.43C92.13,136.98 95.72,139.32 99.13,139.32C102.54,139.32 106.13,136.96 106.13,131.77C106.13,128.77 106.13,124.24 92.13,124.2L92.13,130.43Z" style="fill:rgb(0,205,93);"/>
+        </g>
+    </g>
+</svg>`;
+logo.style.margin = "0";
+logo.style.width = "32px";
+logo.style.maxWidth = "32px";
+logo.style.height = "32px";
+logo.style.maxHeight = "32px";
+title.prepend(logo);
+
 // Add Buy Me a Coffee button
 const buyMeACoffee = document.createElement("span");
 buyMeACoffee.innerHTML = `
   <a target="_blank" href="https://www.buymeacoffee.com/beenyaa">
-    <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=beenyaa&button_colour=00cc99&font_colour=000000&font_family=Inter&outline_colour=000000&coffee_colour=ffffff" />
+    <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=beenyaa&button_colour=009B1C&font_colour=000000&font_family=Inter&outline_colour=000000&coffee_colour=ffffff" />
   </a>`;
 buyMeACoffee.style.margin = "0";
 buyMeACoffee.style.maxWidth = "150px";
@@ -165,11 +187,16 @@ tableBody.addEventListener("click", (event) => {
 keyInputField.addEventListener("input", addNewRowIfNeeded);
 valueInputField.addEventListener("input", addNewRowIfNeeded);
 
-function addNewRowIfNeeded() {
+function addNewRowIfNeeded(key = "") {
   // Check if both input fields in the last row have content
   const lastRow = tableBody.lastElementChild;
   const keyInput = lastRow.cells[1].querySelector("input");
   const valueInput = lastRow.cells[2].querySelector("input");
+
+  if (keyInput.value.trim() == "" && valueInput.value.trim() == "" && key != "") {
+    keyInput.value = key;
+    return
+  }
 
   if (keyInput.value.trim() !== "" || valueInput.value.trim() !== "") {
     // Create a new row and add it to the table
@@ -196,6 +223,7 @@ function addNewRowIfNeeded() {
     );
     newKeyInput.placeholder = "Enter key";
     newKeyInput.style.fontSize = "0.7rem";
+    newKeyInput.value = key;
     newKeyCell.appendChild(newKeyInput);
 
     // Create value input cell
@@ -223,6 +251,29 @@ function addNewRowIfNeeded() {
   }
 }
 
+  // Get references to the textarea element and the table body
+  const textarea = document.querySelector("textarea");
+  
+  // Listen for changes in the textarea element
+  textarea.addEventListener("input", () => {
+    // Get the text from the textarea element
+    const text = textarea.value;
+  
+    // Split the text into parts using the double curly brackets as a delimiter
+    const parts = text.match(/{{(\w+?)}}/g);
+  
+    // Process each part separately
+    for (let i = 0; i < parts.length; i++) {
+      // Extract the parameter name from the odd-indexed parts
+      const paramName = parts[i].replace("{{", "").replace("}}", "");
+      console.log(paramName)
+  
+      if (paramName) {
+        addNewRowIfNeeded(paramName);
+      }
+    }
+  });
+
 // Define the mutation observer callback function
 const observerCallback = (mutationsList, observer) => {
   for (let mutation of mutationsList) {
@@ -232,6 +283,7 @@ const observerCallback = (mutationsList, observer) => {
       const formElement = document.querySelector("form");
       if (mainElement || formElement) {
         mainElement.prepend(contentContainer);
+        // textareaUpdate()
         observer.disconnect();
       }
     }
@@ -247,3 +299,8 @@ observer.observe(document.documentElement, {
   childList: true,
   attributes: true,
 });
+
+
+
+
+
